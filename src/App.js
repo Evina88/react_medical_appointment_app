c2455bbdf07ea9b02ddfc13f4a5609488cf5ecd2
Evina88 <evinamous@gmail.com>
2024-02-17 21:02:26 -0500

    landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+
+
 function App() {
 
   return (
@@ -10,6 +13,7 @@ function App() {
         <BrowserRouter>
           <Navbar/>
               <Routes>
+              <Route path="/" element={<Landing_Page/>}/>
               </Routes>
             
         </BrowserRouter>
