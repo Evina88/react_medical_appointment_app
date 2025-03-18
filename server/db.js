43c77c9633650060f59b538849c78cb90ae039ba
Evina88 <evinamous@gmail.com>
2025-03-18 05:17:50 -0400

    restart

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:ODQzMy1ldmluYW1v@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:kwdIXZmBzuISBvWtZgh8InHT@172.21.168.221:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
